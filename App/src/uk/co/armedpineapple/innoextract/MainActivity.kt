53520b92066a9032a7e7f243b0471416c4765562
Alan Woolley <alan@armedpineapple.co.uk>
2018-01-04 18:48:15 +0000

    Update progress fragment when extracting

--- App/src/uk/co/armedpineapple/innoextract/MainActivity.kt
+++ App/src/uk/co/armedpineapple/innoextract/MainActivity.kt
@@ -1,41 +1,44 @@
 package uk.co.armedpineapple.innoextract
 
 import android.Manifest
-import android.content.*
-import android.net.Uri
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
 import com.karumi.dexter.Dexter
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
-import org.jetbrains.anko.info
-import org.jetbrains.anko.warn
 import uk.co.armedpineapple.innoextract.permissions.PermissionsDialog
 import java.io.File
 
 class MainActivity : SelectorFragment.OnFragmentInteractionListener, ProgressFragment.OnFragmentInteractionListener, IExtractService.ExtractCallback, AnkoLogger, AppCompatActivity() {
 
     override fun onProgress(value: Int, max: Int, speedBps: Int, remainingSeconds: Int) {
-        //info("Success")
+        val progressFragment = supportFragmentManager.findFragmentById(R.id.progressFragment) as? ProgressFragment
+        if (progressFragment != null) {
+            val pct = (1.0f * value/max) * 100
+            progressFragment.update(pct.toInt(), speedBps, remainingSeconds)
+        }
     }
 
     override fun onSuccess() {
-      info("Success")
+        val progressFragment = supportFragmentManager.findFragmentById(R.id.progressFragment) as? ProgressFragment
+        progressFragment?.onExtractFinished()
         showSelectorFragment()
     }
 
     override fun onFailure(e: Exception) {
-        warn("Failure")
+        val progressFragment = supportFragmentManager.findFragmentById(R.id.progressFragment) as? ProgressFragment
+        progressFragment?.onExtractFinished()
         showSelectorFragment()
+
     }
 
-    override fun onFragmentInteraction(uri: Uri) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
     var isServiceBound = false
     var connection = Connection()
@@ -45,9 +48,11 @@ class MainActivity : SelectorFragment.OnFragmentInteractionListener, ProgressFra
 
         supportFragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_out_bottom).hide(supportFragmentManager.findFragmentById(R.id.selectorFragment)).commit()
     }
+
     private fun showSelectorFragment() {
         supportFragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_out_bottom).show(supportFragmentManager.findFragmentById(R.id.selectorFragment)).commit()
     }
+
     inner class Connection : ServiceConnection {
         override fun onServiceDisconnected(className: ComponentName?) {
             debug("Service disconnected")
@@ -110,7 +115,7 @@ class MainActivity : SelectorFragment.OnFragmentInteractionListener, ProgressFra
         debug("Binding service")
         val i = Intent(this, ExtractService::class.java)
         var serviceConnected = bindService(i, connection, Context.BIND_ABOVE_CLIENT or Context.BIND_AUTO_CREATE)
-        debug( "Service connected? : " + serviceConnected)
+        debug("Service connected? : " + serviceConnected)
 
 
         setContentView(R.layout.activity_main)
@@ -140,7 +145,6 @@ class MainActivity : SelectorFragment.OnFragmentInteractionListener, ProgressFra
             finish()
         }
     }
-
-
-
 }
+
+
--- App/src/uk/co/armedpineapple/innoextract/ProgressFragment.kt
+++ App/src/uk/co/armedpineapple/innoextract/ProgressFragment.kt
@@ -6,44 +6,48 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.TextView
+import org.joda.time.Period
+import org.joda.time.format.PeriodFormat
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [ProgressFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [ProgressFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProgressFragment : Fragment() {
-
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
 
     private var mListener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            mParam1 = arguments!!.getString(ARG_PARAM1)
-            mParam2 = arguments!!.getString(ARG_PARAM2)
+    fun update(pct: Int, speedBps: Int, remainingSeconds: Int) {
+        if (!isAdded) {
+            return
         }
+
+        val remainingText = "~ " + PeriodFormat
+                .getDefault().print(Period((remainingSeconds * 1000).toLong())) + " remaining"
+
+        val percentView = view?.findViewById<TextView>(R.id.percentTextView)
+        val remainingView = view?.findViewById<TextView>(R.id.remainingTextView)
+
+        activity?.runOnUiThread({
+            percentView?.visibility = VISIBLE
+            remainingView?.visibility = VISIBLE
+
+            percentView?.text = String.format("%d%%", pct)
+            remainingView?.text = remainingText
+        })
     }
 
+    fun onExtractFinished() {
+        val percentView = view?.findViewById<TextView>(R.id.percentTextView)
+        val remainingView = view?.findViewById<TextView>(R.id.remainingTextView)
+        activity?.runOnUiThread({
+            percentView?.visibility = GONE
+            remainingView?.visibility = GONE
+        })
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progress, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
-    }
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_progress, container, false)
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -59,42 +63,8 @@ class ProgressFragment : Fragment() {
         mListener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
 
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
+    interface OnFragmentInteractionListener
 
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProgressFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): ProgressFragment {
-            val fragment = ProgressFragment()
-            val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
-            return fragment
-        }
-    }
-}// Required empty public constructor
+
+}
